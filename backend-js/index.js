700ba2e657f0c9edbab7e3814570525e93aef1a2
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2025-09-09 23:41:25 +0530

    connect front and back in profile 2

--- backend-js/index.js
+++ backend-js/index.js
@@ -4,7 +4,6 @@ const { syncUser } = require("./middleware/Auth0");
 const { connectMongoDB } = require("./db/connection");
 const { configFunc } = require("./config/authConfig");
 const { auth, requiresAuth } = require("express-openid-connect");
-const session = require("express-session");
 const CompanyRouter = require("./routes/CompanyRouter");
 const UserRouter = require("./routes/UserRouter");
 const cors = require("cors");
@@ -29,20 +28,6 @@ connectMongoDB(mongoURI)
 app.use(jsonParser());
 app.use(configFunc());
 app.use(syncUser);
-app.use(cors());
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || "supersecretkey",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: false, // set true if using https
-      maxAge: 1000 * 60 * 60, // 1 hour
-    },
-  })
-);
 
 app.use("/company", CompanyRouter);
 app.use("/user", UserRouter);
