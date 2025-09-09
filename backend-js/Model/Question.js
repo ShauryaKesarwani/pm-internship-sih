13997767e673de3d6abd28cc212e1903e14f9e04
SohaKhare <sohaajaykhare2006@gmail.com>
2025-09-09 11:09:32 +0530

    Updating Question Schema

--- backend-js/Model/Question.js
+++ backend-js/Model/Question.js
@@ -14,6 +14,10 @@ const questionSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    difficulty:{
+        type: String,
+        required: true
+    },
     weight: {
         type: Number,
     },
